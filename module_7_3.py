d407198b121d8415b478fe2e706caf6366381f25
Nadezhda <61140424+Na6ezh6a@users.noreply.github.com>
2024-11-09 23:09:01 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,34 @@
+# Задача "Найдёт везде".
+import string
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                text = file.read().lower()
+                text = text.translate(str.maketrans('', '', string.punctuation))
+                words = text.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        result = {}
+        all_words = self.get_all_words()
+        for name, words in all_words.items():
+            if word.lower() in words:
+                result[name] = words.index(word.lower()) + 1
+        return result
+
+    def count(self, word):
+        result = {}
+        all_words = self.get_all_words()
+        for name, words in all_words.items():
+            result[name] = words.count(word.lower())
+        return result
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
